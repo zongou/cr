eb0200d5bb2eb71ba8e950f93d5dff4929632042
zongou <zongou@outlook.com>
2025-05-08 11:47:20 +0800

    Added modular files

---
+++ config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern struct config config;
+
+struct config {
+    char *program;
+
+    // Flags
+    int help;
+    int verbose;
+    int all;
+    int markdown;
+    int code;
+
+    // Options
+    char *file_path;
+};
+
+#endif
